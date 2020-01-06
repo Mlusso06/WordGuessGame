faad5b04706558035a716cbb3e46ca7c12775744
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-01-05 22:11:17 -0600

    adding more js code to call out wins, words, and letters on Html

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -3,14 +3,6 @@
 var getRandomNflWord = function() {
 var nflWords = ["catch", "touchdown", "football", "fullback", "running", "quarterback", "titans", "coin toss", "dead ball", "defence", "offence", "placekick", "huddle", "packers", "texans", "radiers", "seahawks", "interception", "turnover", "kickoff", "touchback", "passer", "pitch", "possession", "forward pass"];
 
-
-// We want to start the guessing game with 0 wins, and 12 guesses for the user
-var winsByUser = 0;
-var guessByUser = 12;
-
-// When the user presses the "Start Game button" the run the randome word generator 
-
-
 //function word_picker() {
     var randWord, endWord;
     randWord = Math.floor(Math.random() * nflWords.length);
@@ -35,16 +27,38 @@ for (var i = 0; i < secret_word.length; i++) {
         new_display_word += '_';
     }
 }
+// display the secret word on the Html page
+function displaySecretWord () {
+document.getElementById("secretWord").innerHTML = "The Current word is: " + display_word;
+
+}
 // checking to see the function returns a random Nfl word //
 console.log(secret_word);
 console.log(display_word);
 console.log(new_display_word);
 
+// We want to start the guessing game with 0 wins, and 12 guesses for the user
+var winsByUser = 0;
+var guessByUser = 12;
+
+// When the user presses the "Start Game button" the run the randome word generator 
+function startGame() {
+
+
+
+// creating a var to hold the text for the wins and guesses
+var winsText = document.getElementById("winsByUser");
+
+//display the wins and guesses
+winsText.textContent = "User Wins:" + winsByUser;
+}
 
 // This is to update the wins if user guesses correctly
-function updateScore() {
-    document.querySelector("#winsByUser").innerHTML = "Score: " + score;
-  }
+// function userWins() {
+//     document.querySelector("#winsByUser").innerHTML = "Score: " + winsByUser;
+//   }
+
+//   userWins();
 
 
 
