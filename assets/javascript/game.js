84ab0c043a60adf47c829d982023b07c0fdc9733
spe1901 <52164194+Mlusso06@users.noreply.github.com>
2020-01-09 19:00:49 -0600

    changes related to alerts and secret words display

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -6,6 +6,11 @@ document.addEventListener("DOMContentLoaded", function () {
     // Nfl words to use for Football Hangman //
     var keyword = "";
     var new_display_word = '';
+    var corLet = [];
+    var wrongLet = [];
+    var winsByUser = 0;
+    var guessByUser = 12;
+
     var getRandomNflWord = function () {
         var nflWords = ["catch", "touchdown", "football", "fullback", "running", "quarterback", "titans", "cointoss", "deadball", "defence", "offence", "placekick", "huddle", "packers", "texans", "radiers", "seahawks", "interception", "turnover", "kickoff", "touchback", "passer", "pitch", "possession", "forwardpass"];
 
@@ -14,6 +19,7 @@ document.addEventListener("DOMContentLoaded", function () {
         randWord = Math.floor(Math.random() * nflWords.length);
         endWord = nflWords[randWord];
         return endWord;
+
 
     }
     // this is to make the random word appear as a  _ _ _ _ based on the letters in word
@@ -27,7 +33,27 @@ document.addEventListener("DOMContentLoaded", function () {
     //Capture the user's guess by letters pressed, and display the letters
     document.addEventListener('keyup', (event) => {
         keyword = event.key;
+        // this is to check if the user is a correct 
+        if (secret_word.indexOf(keyword) > -1) {
+            // now push the correct letters to the corLet 
+            corLet.push(keyword);
+            console.log(corLet);
+            // trying to replace the word with the underscore properly
+            display_word[secret_word.indexOf(keyword)] = keyword;
+            // does the display word match the secret word?
+            if(display_word.join('') == secret_word) {
+                // I want to change the alert to a ++ to the var wins by user
+                alert('you Win');
+
+            }
+        }    
+        else {
+            wrongLet.push(keyword);
+            console.log(wrongLet)
+        }
         console.log(keyword)
+
+
         // for loop to run throught the word and check it against the key stroke
         for (var i = 0; i < secret_word.length; i++) {
             if (secret_word[i] === keyword) {
@@ -45,7 +71,7 @@ document.addEventListener("DOMContentLoaded", function () {
     // display the secret word on the Html page with _____ to show the # of letters in the hint "should look like _ _ _ _ _"
 
     document.getElementById("secretWord").innerHTML = "The Current word is: " + display_word;
-// Need code to check against the secret word, and then if they are not in the word it should display in the "wrongletter" on the html box
+    // Need code to check against the secret word, and then if they are not in the word it should display in the "wrongletter" on the html box
 
 
 
